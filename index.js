3c592b44db2b410221b84045193296ab4426f314
tommygebru <gebru.t@gmail.com>
2020-05-28 18:19:27 -0700

    pull default query

--- index.js
+++ index.js
@@ -4,4 +4,17 @@ $(document).ready(function() {
 	******************************************/
 	$(".search-bar").on("click", "#search-btn", newQuery);
 	const apiKey = "628ca4460ecf460faaa076684f9f59d7";
+  /******************************************
+  	P U L L   D E F A U L T   Q U E R Y
+  	******************************************/
+  	function getNews() {
+  		fetch("https://newsapi.org/v2/everything?q=tesla&language=en&apiKey=628ca4460ecf460faaa076684f9f59d7").then(response => {
+  			if (response.ok) {
+  				return response.json();
+  			} else {
+  				throw console.log(response.text());
+  			}
+  		}).then(responseJson => displaySearchResults(responseJson)).catch(error => displaySearchError(error));
+  	}; //function
+  	getNews();
 });
