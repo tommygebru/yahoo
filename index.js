dc61ba1b56dbfc6f9777f6d04a82e3d2578772a4
tommygebru <gebru.t@gmail.com>
2020-05-28 18:01:59 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+	/******************************************
+	C L I C K   E V E N T
+	******************************************/
+	$(".search-bar").on("click", "#search-btn", newQuery);
+	const apiKey = "628ca4460ecf460faaa076684f9f59d7";
+});
